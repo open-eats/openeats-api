c180cb7c3b2898ba8e7061535f25546206897dea
ryan.noelk <ryannoelk@gmail.com>
2018-03-19 12:10:23 +0100

    fixing tests

--- v1/accounts/tests.py
+++ v1/accounts/tests.py
@@ -2,6 +2,9 @@
 # encoding: utf-8
 
 from django.test import TestCase
+from rest_framework_jwt.settings import api_settings
+
+jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 
 
 class AccountTests(TestCase):
@@ -18,7 +21,9 @@ class AccountTests(TestCase):
 
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(resp.json()['id'] == 1)
-        self.assertTrue(len(resp.json()['token']) == 40)
+        decoded_token = jwt_decode_handler(resp.json()['token'])
+        self.assertTrue(decoded_token.get('id') == 1)
+        self.assertTrue(decoded_token.get('username') == 'testuser1')
 
     def test_obtain_authtoken_wrong_password(self):
         resp = self.client.post(
