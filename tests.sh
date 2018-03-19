94330a68aa082de23fbefcadcf99858629b3bf7b
ryan.noelk <ryannoelk@gmail.com>
2018-03-20 00:51:56 +0100

    adding repo token

--- tests.sh
+++ tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
 pip install coveralls==1.3.0
-env
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+
 python manage.py reset_db --noinput
 python manage.py migrate
 coverage run manage.py test -k
 
+./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
