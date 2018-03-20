625fc8ce0401395b5c1537adb630e08e32d8e560
ryan.noelk <ryannoelk@gmail.com>
2018-03-20 01:07:15 +0100

    test

--- tests.sh
+++ tests.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env sh
 
 pip install coveralls==1.3.0
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x ./cc-test-reporter
-./cc-test-reporter before-build
-
 python manage.py reset_db --noinput
 python manage.py migrate
 coverage run manage.py test -k
-
-./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
