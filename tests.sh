edcb28d9f6157ac0200d1f1eba81c349ddbbf17c
Ryan Noelk <ryannoelk@gmail.com>
2018-03-27 20:52:10 +0200

    Fixing issue with tests always passing (#8)
    * test
    
    * test
    
    * fixing test
    
    * fail tests if they fail
    
    * checking passed tests now
    
    * checking passed tests now
    
    * adding failed tests
    
    * fixing tests again

--- tests.sh
+++ tests.sh
@@ -10,6 +10,10 @@ python manage.py migrate
 
 # Run the tests and create a coverage report
 coverage run --omit="*/migrations*,*/fixtures*" manage.py test -k
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit 1
+fi
 
 # Submit coverage to Coveralls
 coveralls
