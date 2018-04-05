bea348988de9fb95e1732960abedd3c618ad05b8
ryan.noelk <ryannoelk@gmail.com>
2018-04-05 23:05:24 +0200

    adding tests and adding timezones to fixtures

---
+++ v1/common/tests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from django.test import TestCase
+from v1.recipe.models import Recipe
+from v1.common.recipe_search import get_search_results
+
+
+class GetSearchResultsTests(TestCase):
+    fixtures = [
+        'test/users.json',
+        'course_data.json',
+        'cuisine_data.json',
+        'ing_data.json',
+        'recipe_data.json'
+    ]
+
+    def test_get_search_results(self):
+        query = get_search_results(
+            ['title', 'ingredient_groups__ingredients__title', 'tags__title'],
+            Recipe.objects,
+            'chili'
+        ).distinct()
+
+        self.assertTrue(len(query.all()) > 0)
+
+    def test_get_search_no_results(self):
+        query = get_search_results(
+            ['title', 'ingredient_groups__ingredients__title', 'tags__title'],
+            Recipe.objects,
+            'blue berry'
+        ).distinct()
+
+        self.assertTrue(len(query.all()) == 0)
