fa6d698e9ef079eccdce7fef5501e1bcf0fb8c32
ryannoelk <ryannoelk@gmail.com>
2018-11-17 11:07:47 -0500

    adding some comments

--- v1/recipe/save_recipe.py
+++ v1/recipe/save_recipe.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from django.db.models import Count
 from django.core.exceptions import FieldDoesNotExist
 from v1.recipe.models import Recipe, SubRecipe
 from v1.recipe_groups.models import Tag, Course, Cuisine
@@ -9,8 +10,8 @@ from v1.ingredient.models import IngredientGroup, Ingredient
 
 class SaveRecipe(object):
     def __init__(self, data, author, instance=None):
+        # Extract the data from data for use in the saving process
         self.author = author
-
         self.ingredients = data.pop('ingredient_groups', None)
         self.tags = data.pop('tags', None)
         self.course = data.pop('course', None)
@@ -18,12 +19,14 @@ class SaveRecipe(object):
         self.subrecipes = data.pop('subrecipes', None)
         self.data = data
 
+        # Save the data
         self._save_course()
         self._save_cuisine()
         self.recipe = self.update(instance) if instance else self.create()
         self._save_ingredient_data()
         self._save_subrecipe_data()
         self._save_tags()
+        self._delete_recipe_groups()
 
     def _save_course(self):
         if self.course:
@@ -44,6 +47,12 @@ class SaveRecipe(object):
                     title=self.cuisine.get('title'),
                     defaults={'author': self.author},
                 )
+
+    def _delete_recipe_groups(self):
+            # Check to see if we have any Cuisines or Courses with no recipes associated with them.
+            # Id we do, delete them.
+            Cuisine.objects.all().annotate(total=Count('recipe', distinct=True)).filter(total=0).delete()
+            Course.objects.all().annotate(total=Count('recipe', distinct=True)).filter(total=0).delete()
 
     def _save_tags(self):
         if self.tags:
@@ -91,6 +100,10 @@ class SaveRecipe(object):
                         obj.save()
 
     def clean_data(self):
+        """
+        Clean up the data before we try and save it into the Recipe model
+        :return:
+        """
         for key in ['author', 'id', 'slug']:
             self.data.pop(key) if self.data.get(key) is not None else None
 
